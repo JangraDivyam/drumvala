11817dba58782b4f0cbac28f0fa0f83a9ab02b84
JangraDivyam <143521790+JangraDivyam@users.noreply.github.com>
2025-02-04 18:27:04 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -5,37 +5,37 @@ document.addEventListener("keypress",function(event){
 function playsound(key){
     switch(key){
         case "w":
-        var s1=new Audio("./sounds/crash.mp3");
+        var s1=new Audio("./crash.mp3");
         s1.play();
         break;
         
         case "a":
-        var s2=new Audio("./sounds/kick-bass.mp3");
+        var s2=new Audio("./kick-bass.mp3");
         s2.play();
         break;
 
         case "s":
-        var s3=new Audio("./sounds/snare.mp3");
+        var s3=new Audio("./snare.mp3");
         s3.play();
         break;
 
         case "d":
-        var s4=new Audio("./sounds/tom-1.mp3");
+        var s4=new Audio("./tom-1.mp3");
         s4.play();
         break;
 
         case "j":
-        var s5=new Audio("./sounds/tom-2.mp3");
+        var s5=new Audio("./tom-2.mp3");
         s5.play();
         break;
 
         case "k":
-        var s6=new Audio("./sounds/tom-3.mp3");
+        var s6=new Audio("./tom-3.mp3");
         s6.play();
         break;
 
         case "l":
-        var s7=new Audio("./sounds/tom-4.mp3");
+        var s7=new Audio("./tom-4.mp3");
         s7.play();
         break;
 
